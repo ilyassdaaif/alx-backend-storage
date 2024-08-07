c139b61c1d1543c149b550691dd6885c83b5dbb9
ilyass <ilyass.daaif@gmail.com>
2024-08-07 23:34:37 +0000

    a

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- List Glam rock bands ranked by longevity
-SELECT band_name, IFNULL(2022, - formed, 0) AS lifespan
+SELECT band_name, 
+       IFNULL(2022 - formed, 0) AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', style) > 0
 ORDER BY lifespan DESC;
