e2162e1ae3eebe35a00d808489e97c237018f66b
ilyass <ilyass.daaif@gmail.com>
2024-08-08 00:48:31 +0000

    s

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -7,12 +7,6 @@ BEGIN
     UPDATE items
     SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
-    
-    -- If no rows were affected (item doesn't exist), insert a new item with negative quantity
-    IF ROW_COUNT() = 0 THEN
-        INSERT INTO items (name, quantity)
-        VALUES (NEW.item_name, -NEW.number);
-    END IF;
 END//
 
 DELIMITER ;
