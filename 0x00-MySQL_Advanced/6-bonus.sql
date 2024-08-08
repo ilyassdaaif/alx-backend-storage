87b551d62caa115a1c7278c7f20b956ecb9bf00a
ilyass <ilyass.daaif@gmail.com>
2024-08-08 22:27:51 +0000

    a

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,35 @@
+DELIMITER //
+
+CREATE PROCEDURE AddBonus(
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score INT
+)
+BEGIN
+    DECLARE project_id INT;
+
+    -- check if the project already exists, if not, create it
+    SELECT id INTO project_id    
+    FROM projects
+    WHERE name = project_name;
+
+    IF project_id IS NULL THEN
+	INSERT INTO projects (name) VALUES (project_name);
+	SET project_id = LAST_INSERT_ID();
+    END IF;
+
+    -- Insert the new correction
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, project_id, score);
+
+    -- Update the user's average score
+    UPDATE users
+    SET average_score = (
+	SELECT AVG(score)
+	FROM corrections
+	WHERE user_id = AddBonus.user_id
+    )
+    WHERE id = AddBonus.user_id;
+END//
+	
+DELIMITER ;
