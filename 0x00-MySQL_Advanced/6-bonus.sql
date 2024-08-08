1a8b5420cc221502a207b719db24a3898ad25f16
ilyass <ilyass.daaif@gmail.com>
2024-08-08 22:55:53 +0000

    s

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,41 +1,23 @@
--- Create the AddBonus stored procedure
+-- Stored Procedure to Add Bonus Corrections
+DROP PROCEDURE IF EXISTS AddBonus;
+
 DELIMITER //
 
-CREATE PROCEDURE AddBonus(
-    IN user_id INT,
-    IN project_name VARCHAR(255),
-    IN score INT
-)
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
     DECLARE project_id INT;
 
-    -- Check if the user exists
-    IF (SELECT COUNT(*) FROM users WHERE id = user_id) = 0 THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'User does not exist';
-    END IF;
+    -- Check if the project already exists
+    SET project_id = (SELECT id FROM projects WHERE name = project_name);
 
-    -- Check if the project already exists, if not, create it
-    SELECT id INTO project_id
-    FROM projects
-    WHERE name = project_name;
-
+    -- If the project doesn't exist, create it
     IF project_id IS NULL THEN
         INSERT INTO projects (name) VALUES (project_name);
         SET project_id = LAST_INSERT_ID();
     END IF;
 
-    -- Insert the new correction
-    INSERT INTO corrections (user_id, project_id, score)
-    VALUES (user_id, project_id, score);
-
-    -- Update the user's average score
-    UPDATE users
-    SET average_score = (
-        SELECT AVG(score)
-        FROM corrections
-        WHERE user_id = AddBonus.user_id
-    )
-    WHERE id = AddBonus.user_id;
-END//
+    -- Insert the correction into the corrections table
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+END //
 
 DELIMITER ;
