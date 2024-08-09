fab76235125ca7c0985b2d21f97fed55d16f547a
ilyass <ilyass.daaif@gmail.com>
2024-08-09 00:17:36 +0000

    s

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,22 @@
+-- Drop the procedure if it already exists
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+DELIMITER //
+
+-- Create the ComputeAverageScoreForUser procedure
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score FLOAT;
+
+    -- Calculate the average score for the specified user
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE user_id = user_id;
+
+    -- Update the user's average_score in the users table
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id;
+END //
+
+DELIMITER ;
