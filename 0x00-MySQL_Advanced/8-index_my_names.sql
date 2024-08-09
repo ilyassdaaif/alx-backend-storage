e353d620353d3a2eadc6b25f6b048b67ce85352e
ilyass <ilyass.daaif@gmail.com>
2024-08-09 00:58:41 +0000

    a

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,5 @@
--- Drop the index if it already exists
+-- Drop the index if you want to recreate it
 DROP INDEX IF EXISTS idx_name_first ON names;
 
--- Create the index on the first character of the name column
+-- Create the index on the first letter of the name
 CREATE INDEX idx_name_first ON names (name(1));
