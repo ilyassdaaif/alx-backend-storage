e24e847c81667ea24e1e886bc4758ae75408e0ef
ilyass <ilyass.daaif@gmail.com>
2024-08-13 22:25:28 +0000

    q

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Changes all topics of a school document based on the name"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name.
+
+    Args:
+        mongo_collection: The pymongo collection object.
+        name (str): The school name to update.
+        topics (list of str): The list of topics approached in the school.
+
+    Returns:
+        None
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
