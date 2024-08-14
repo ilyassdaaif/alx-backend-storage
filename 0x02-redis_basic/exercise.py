4535e4a733a8ea4258d7b8cdab78f295bf8e0e55
ilyassdf <ilyassdf@gmail.com>
2024-08-14 21:52:09 +0000

    origin

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -50,8 +50,8 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None)
-    -> Union[str, bytes, int, float, None]:
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union
+        [str, bytes, int, float, None]:
         """
         Retrieve data from Redis and optionally convert it using fn.
         """
