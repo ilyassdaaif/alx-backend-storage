e805a5af8a42f9ba80c0888be310b2f6d5384afe
ilyassdf <ilyass@gmail.com>
2024-08-15 14:28:11 +0000

    z

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Returns all students sorted by average score."""
+
+
+def top_students(mongo_collection):
+    """Return all students sorted by average score."""
+    pipeline = [
+        {
+            "$project": {
+                "name": 1,
+                "averageScore": {
+                    "$avg": "$topics.score"
+                }
+            }
+        },
+        {
+            "$sort": {
+                "averageScore": -1
+            }
+        }
+    ]
+
+    return list(mongo_collection.aggregate(pipeline))
